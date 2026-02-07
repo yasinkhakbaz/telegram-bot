79b4cc7209112a4d0439a6034e1eb97dc759297e
yasinkhakbaz <yasinkhakbaz2@gmail.com>
2026-02-07 14:02:11 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 ربات با موفقیت روی GitHub فعاله ✅")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
